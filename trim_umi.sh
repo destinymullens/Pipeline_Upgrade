737b2e5232759533cbf8ecafbc8349be079fee38
destinymullens <destiny_mullens@tamu.edu>
2022-01-04 22:38:23 +0000

    Changes made to correct errors, change folders output is saved in and add output dialogue.

--- trim_umi.sh
+++ trim_umi.sh
@@ -5,15 +5,29 @@
 # Read config.sh
 . $(dirname $0)/config.sh
 
-mkdir -p $SAVE_LOC/$project_name/trimmed_files/$trim_type
+mkdir -p $SAVE_LOC/$project_name/trimmed_files/$trim_type/barcodes_removed
+mkdir -p $SAVE_LOC/$project_name/trimmed_files/$trim_type/barcodes_removed/logfiles
+mkdir -p $SAVE_LOC/$project_name/trimmed_files/$trim_type/final_trim
+
 trim_dir_in="$SAVE_LOC/$project_name/concat"
-trim_dir_out="$SAVE_LOC/$project_name/trimmed_files/$trim_type"
+trim_dir_out="$SAVE_LOC/$project_name/trimmed_files/$trim_type/barcodes_removed"
+trim_final_dir="$SAVE_LOC/$project_name/trimmed_files/$trim_type/final_trim"
 
 SAMPLES=$(find $trim_dir_in -type f -printf '%f\n')
 
 for s in $SAMPLES; do
 	logfile="$s.processed.log"
-	stoutfile="$s.processed.stout.log"
-	$UMI_TOOLS extract --bc-pattern=NNNNNN --stdin $trim_dir_in/$s --log $trim_dir_out/$logfile --stout $trim_dir_out/$stoutfile
-	$CUTADAPT -u 4 -o $trim_dir_out/$samplename.trimm.fastq.gz $trim_dir_in/$s
+	stoutfile="$s.processed.fastq.gz"
+	echo "Extracting UMI's from $s."
+	$UMI_TOOLS extract --bc-pattern=NNNNNN -I $trim_dir_in/$s --log $trim_dir_out/logfiles/$logfile -S $trim_dir_out/$stoutfile
+	echo "Extracting UMI's from $s is now complete."
+done
+
+SAMPLES2=$(find $trim_dir_out -type f -printf '%f\n')
+
+for s in $SAMPLES2; do
+	filename="basename $s"
+	echo "Trimming bases from $s."
+	$CUTADAPT -u 4 -o $trim_final_dir/$filename.trimm.fastq.gz $trim_dir_out/$s
+	echo "Trimming complete for $s."
 done
