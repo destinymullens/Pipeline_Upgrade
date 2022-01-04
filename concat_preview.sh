75cfa64a97f4b30ed08c52c2565464e75b6fbfe7
destinymullens <destiny_mullens@tamu.edu>
2022-01-04 22:36:51 +0000

    Change concat from bz2 to gz output.

--- concat_preview.sh
+++ concat_preview.sh
@@ -10,7 +10,7 @@ SAMPLES=$(find $file_location -type f -printf '%f\n' | cut -c 1-$concat_length |
 OUT="$SAVE_LOC/$project_name/concat"
 
 for s in $SAMPLES; do
-	NEW=$OUT/$s.fastq.bz2
+	NEW=$OUT/$s.fastq.gz
 	if [[ ! -s $NEW ]]; then
 		FILES=$(find $file_location -iname "$s"*.gz)
 		echo "For sample $s, concatenating input files:"
