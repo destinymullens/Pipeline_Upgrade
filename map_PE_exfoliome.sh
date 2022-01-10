14cc51c223114fbd8029a6529801453c66f4ad2a
Destiny Mullens <destiny_mullens@tamu.edu>
2022-01-10 16:14:57 -0600

    Update and rename map_PE_exfoliome.sh to map_exfoliome.sh

--- map_PE_exfoliome.sh
+++
@@ -1,3 +0,0 @@
-##!/bin/bash
-
-echo "Mapping paired-end exfoliome now.."
---
+++ map_exfoliome.sh
@@ -0,0 +1,71 @@
+##!/bin/bash
+
+#!/bin/bash
+
+## This script is to perform test mappings for exfoliome samples
+
+# Read config.sh
+. $(dirname $0)/config.sh
+
+## Select random files for test mapping
+
+mkdir -p $SAVE_LOC/$project_name/mapping
+mkdir -p $SAVE_LOC/$project_name/mapping/logs
+mapping_out="$SAVE_LOC/$project_name/mapping"
+mapping_logs="$SAVE_LOC/$project_name/mapping/logs"
+mappings=$(ls $mapfiles)
+SUMMARY="$mapping_logs/Mapping_results.txt"
+
+
+mp_input="$exfoliome_mapping_parameter | cut 2"
+ma_input="$exfoliome_mapping_parameter | cut 5"
+
+
+##### RUN BOWTIE2 #########
+
+
+for m in $mappings; do
+
+	FILE=$(basename $m)
+
+	A=mp_input
+	B=ma_input
+
+	## Additional options can be added for the -mp and -ma mappings if preferred, but the number of loops needs to be changed if other options are added
+	mp_options=(6 4 2)
+	ma_options=(2 6 8)
+
+	## The number of loops here (3) should be the same as the number of options being tested.
+		mp=$(echo ${mp_options[A]})
+		
+		## The number of loops here (3) should be the same as the number of options being tested.
+		while [ $B -lt 3 ]
+		do
+		ma=$(echo ${ma_options[B]})
+	
+		## This designates mapping "D" for the first option (mp) and option "F" for the second option (ma)
+		## The options will be 0, 1 & 2 for the 3 options given above for each parameter
+		MAPPING="D$A-F$B"
+
+		printf "%s\n" "Mapping $FILE with $MAPPING mapping options beginning..."
+
+		$BOWTIE -x $species_location/bowtie2/$species --threads $THREADS -U $mapfiles/$m --mp $mp --ma $ma --local --time -S $test_map_out/$FILE-$MAPPING.sam 2> $test_map_logs/$FILE-$MAPPING-Results.log
+
+		printf "%s\n" "Mapping $FILE with $MAPPING mapping options finished."
+		
+		## Cut info from each log file for comparision to see which is the best mapping option
+		TOTAL_READS=$(cat $test_map_logs/$FILE-$MAPPING-Results.log | head -5 | tail -1 | cut -c 1-9)
+		SINGLE_MAPPED_READS=$(cat $test_map_logs/$FILE-$MAPPING-Results.log | head -8 | tail -1 | cut -c 5-12)
+		UNMAPPED_READS=$(cat $test_map_logs/$FILE-$MAPPING-Results.log | head -7 | tail -1 | cut -c 5-12)
+		MULTI_MAP_READS=$(cat $test_map_logs/$FILE-$MAPPING-Results.log | head -9 | tail -1 | cut -c 5-12)
+		ALIGNMENT_RATE=$(cat $test_map_logs/$FILE-$MAPPING-Results.log | head -10 | tail -1 | cut -c 1-6)
+
+		printf "%s\t" "$FILE" >> $SUMMARY ## Print sample name to summary	
+		printf "%s\t" "$MAPPING" >> $SUMMARY ## Print mapping to summary
+		printf "%s\t" "$TOTAL_READS" >> $SUMMARY ## Print Total reads to summary
+		printf "%s\t" "$SINGLE_MAPPED_READS" >> $SUMMARY ## Print single mapped reads to summary
+		printf "%s\t" "$UNMAPPED_READS" >> $SUMMARY ## Print unmapped reads to summary 
+		printf "%s\t" "$MULTI_MAP_READS" >> $SUMMARY ## Print multimapped reads to summary
+		printf "%s\n" "$ALIGNMENT_RATE" >> $SUMMARY ## Print alignment rate to summary
+done
+
