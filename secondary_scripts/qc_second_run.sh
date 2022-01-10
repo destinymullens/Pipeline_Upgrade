3e2eeec41d902e6ef5fccc7c1d7f5fe3f5603efb
destinymullens <destiny_mullens@tamu.edu>
2022-01-10 18:02:17 +0000

    Adding folder of secondary scripts

---
+++ secondary_scripts/qc_second_run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+## This script is to run QC Reports
+# Read config.sh
+. $(dirname $0)/config.sh
+
+SAMPLES=$(find $mapfiles -type f -printf '%f\n')
+echo "$SAMPLES"
+mkdir -p "$SAVE_LOC/$project_name/qc_reports_after_trim"
+results="$SAVE_LOC/$project_name/qc_reports_after_trim"
+for s in $SAMPLES; do
+samplename="${s%%.*}"
+	if [[ ! -d $results/$samplename ]]; then
+		echo "$samplename QC Report currently running."
+		mkdir -p $results/$samplename
+		$FASTQC $mapfiles/$s -o $results/$samplename -t 50
+		echo ""
+	fi
+done
