e9cd3b0a0eeb62cc5ae53987afd0ccf9dcc590ed
Destiny Mullens <destiny_mullens@tamu.edu>
2022-01-10 12:39:11 -0600

    Update htseq.sh

--- htseq.sh
+++ htseq.sh
@@ -5,7 +5,7 @@
 
 mkdir -p "$SAVE_LOC/$project_name/summary/htseq_counts"
 htseq_dir_in="$SAVE_LOC/$project_name/mapping"
-htseq_dir_out=$SAVE_LOC/$project_name/summary/htseq_counts"
+htseq_dir_out="$SAVE_LOC/$project_name/summary/htseq_counts"
 
 if [[ "$strand_num" = "1" ]]; 
 	for i in $htseq_dir_in; do
