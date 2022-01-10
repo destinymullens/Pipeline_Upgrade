039112b10b044e56f6b25a03a3b6c05c70e73cce
Destiny Mullens <destiny_mullens@tamu.edu>
2022-01-10 16:24:05 -0600

    Update map_exfoliome.sh

--- map_exfoliome.sh
+++ map_exfoliome.sh
@@ -35,37 +35,29 @@ for m in $mappings; do
 	mp_options=(6 4 2)
 	ma_options=(2 6 8)
 
-	## The number of loops here (3) should be the same as the number of options being tested.
-		mp=$(echo ${mp_options[A]})
-		
-		## The number of loops here (3) should be the same as the number of options being tested.
-		while [ $B -lt 3 ]
-		do
-		ma=$(echo ${ma_options[B]})
+	mp=$(echo ${mp_options[A]})
+	ma=$(echo ${ma_options[B]})
 	
-		## This designates mapping "D" for the first option (mp) and option "F" for the second option (ma)
-		## The options will be 0, 1 & 2 for the 3 options given above for each parameter
-		MAPPING="D$A-F$B"
+	MAPPING="D$A-F$B"
 
-		printf "%s\n" "Mapping $FILE with $MAPPING mapping options beginning..."
+	printf "%s\n" "Mapping $FILE with $MAPPING mapping options beginning..."
 
-		$BOWTIE -x $species_location/bowtie2/$species --threads $THREADS -U $mapfiles/$m --mp $mp --ma $ma --local --time -S $test_map_out/$FILE-$MAPPING.sam 2> $test_map_logs/$FILE-$MAPPING-Results.log
-
-		printf "%s\n" "Mapping $FILE with $MAPPING mapping options finished."
+	$BOWTIE -x $species_location/bowtie2/$species --threads $THREADS -U $mapfiles/$m --mp $mp --ma $ma --local --time -S $test_map_out/$FILE-$MAPPING.sam 2> $test_map_logs/$FILE-$MAPPING-Results.log
+	
+	printf "%s\n" "Mapping $FILE with $MAPPING mapping options finished."
 		
-		## Cut info from each log file for comparision to see which is the best mapping option
-		TOTAL_READS=$(cat $test_map_logs/$FILE-$MAPPING-Results.log | head -5 | tail -1 | cut -c 1-9)
-		SINGLE_MAPPED_READS=$(cat $test_map_logs/$FILE-$MAPPING-Results.log | head -8 | tail -1 | cut -c 5-12)
-		UNMAPPED_READS=$(cat $test_map_logs/$FILE-$MAPPING-Results.log | head -7 | tail -1 | cut -c 5-12)
-		MULTI_MAP_READS=$(cat $test_map_logs/$FILE-$MAPPING-Results.log | head -9 | tail -1 | cut -c 5-12)
-		ALIGNMENT_RATE=$(cat $test_map_logs/$FILE-$MAPPING-Results.log | head -10 | tail -1 | cut -c 1-6)
+	## Cut info from each log file for comparision to see which is the best mapping option
+	TOTAL_READS=$(cat $test_map_logs/$FILE-$MAPPING-Results.log | head -5 | tail -1 | cut -c 1-9)
+	SINGLE_MAPPED_READS=$(cat $test_map_logs/$FILE-$MAPPING-Results.log | head -8 | tail -1 | cut -c 5-12)
+	UNMAPPED_READS=$(cat $test_map_logs/$FILE-$MAPPING-Results.log | head -7 | tail -1 | cut -c 5-12)
+	MULTI_MAP_READS=$(cat $test_map_logs/$FILE-$MAPPING-Results.log | head -9 | tail -1 | cut -c 5-12)
+	ALIGNMENT_RATE=$(cat $test_map_logs/$FILE-$MAPPING-Results.log | head -10 | tail -1 | cut -c 1-6)
 
-		printf "%s\t" "$FILE" >> $SUMMARY ## Print sample name to summary	
-		printf "%s\t" "$MAPPING" >> $SUMMARY ## Print mapping to summary
-		printf "%s\t" "$TOTAL_READS" >> $SUMMARY ## Print Total reads to summary
-		printf "%s\t" "$SINGLE_MAPPED_READS" >> $SUMMARY ## Print single mapped reads to summary
-		printf "%s\t" "$UNMAPPED_READS" >> $SUMMARY ## Print unmapped reads to summary 
-		printf "%s\t" "$MULTI_MAP_READS" >> $SUMMARY ## Print multimapped reads to summary
-		printf "%s\n" "$ALIGNMENT_RATE" >> $SUMMARY ## Print alignment rate to summary
+	printf "%s\t" "$FILE" >> $SUMMARY ## Print sample name to summary	
+	printf "%s\t" "$MAPPING" >> $SUMMARY ## Print mapping to summary
+	printf "%s\t" "$TOTAL_READS" >> $SUMMARY ## Print Total reads to summary
+	printf "%s\t" "$SINGLE_MAPPED_READS" >> $SUMMARY ## Print single mapped reads to summary
+	printf "%s\t" "$UNMAPPED_READS" >> $SUMMARY ## Print unmapped reads to summary 
+	printf "%s\t" "$MULTI_MAP_READS" >> $SUMMARY ## Print multimapped reads to summary
+	printf "%s\n" "$ALIGNMENT_RATE" >> $SUMMARY ## Print alignment rate to summary
 done
-
