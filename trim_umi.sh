7ebce08633c8d93d623f04c53c4a780cfbaac2ff
Destiny Mullens <destiny_mullens@tamu.edu>
2022-01-11 14:51:38 -0600

    Update trim_umi.sh

--- trim_umi.sh
+++ trim_umi.sh
@@ -6,13 +6,14 @@
 . $(dirname $0)/config.sh
 
 mkdir -p $SAVE_LOC/$project_name/trimmed_files/$trim_type/barcodes_removed
-mkdir -p $SAVE_LOC/$project_name/trimmed_files/$trim_type/barcodes_removed/logfiles
 mkdir -p $SAVE_LOC/$project_name/trimmed_files/$trim_type/final_trim
+mkdir -p $SAVE_LOC/$project_name/logs
+mkdir -p $SAVE_LOC/$project_name/logs/$trim_type/barcodes_removal
 
 trim_dir_in="$SAVE_LOC/$project_name/concat"
 trim_dir_out="$SAVE_LOC/$project_name/trimmed_files/$trim_type/barcodes_removed"
 trim_final_dir="$SAVE_LOC/$project_name/trimmed_files/$trim_type/final_trim"
-trim_log="$SAVE_LOC/$project_name/trimmed_files/$trim_type/barcodes_removed/logfiles"
+trim_log="$SAVE_LOC/$project_name/logs/$trim_type/barcodes_removal"
 SAMPLES=$(find $trim_dir_in -type f -printf '%f\n')
 
 for s in $SAMPLES; do
