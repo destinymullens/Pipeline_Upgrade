1edfbd6aea664447090fb7bd17686c7c72b2d0d6
Destiny Mullens <destiny_mullens@tamu.edu>
2022-01-11 17:22:10 -0600

    Update trim_umi.sh

--- trim_umi.sh
+++ trim_umi.sh
@@ -5,15 +5,14 @@
 # Read config.sh
 . $(dirname $0)/config.sh
 
-mkdir -p $SAVE_LOC/$project_name/trimmed_files/$trim_type/barcodes_removed
-mkdir -p $SAVE_LOC/$project_name/trimmed_files/$trim_type/final_trim
+mkdir -p $SAVE_LOC/$project_name/trimmed_files/$trim_type
 mkdir -p $SAVE_LOC/$project_name/logs
-mkdir -p $SAVE_LOC/$project_name/logs/$trim_type/barcodes_removal
+mkdir -p $SAVE_LOC/$project_name/logs/$trim_type
 
 trim_dir_in="$SAVE_LOC/$project_name/concat"
-trim_dir_out="$SAVE_LOC/$project_name/trimmed_files/$trim_type/barcodes_removed"
-trim_final_dir="$SAVE_LOC/$project_name/trimmed_files/$trim_type/final_trim"
+trim_dir_out="$SAVE_LOC/$project_name/trimmed_files/$trim_type"
 trim_log="$SAVE_LOC/$project_name/logs/$trim_type/barcodes_removal"
+
 SAMPLES=$(find $trim_dir_in -type f -printf '%f\n')
 
 for s in $SAMPLES; do
@@ -21,20 +20,8 @@ for s in $SAMPLES; do
 	if [[ ! -d $trim_dir_out/$samplename ]]; then
 		logfile="$samplename.processed.log"
 		stoutfile="$samplename.processed.fastq.gz"
-		echo "Extracting UMI's from $s."
-		$UMI_TOOLS extract --bc-pattern=NNNNNN -I $trim_dir_in/$s --log $trim_log/$logfile -S $trim_dir_out/$stoutfile
-		echo "Extracting UMI's from $s is now complete."
+		echo "Begining trimming of $s...."
+		$UMI_TOOLS extract --bc-pattern=NNNNNN -I $trim_dir_in/$s --log $trim_log/$logfile | $CUTADAPT -u 4 -J 4 -o $trim_final_dir/$samplename.trimm.fastq.gz -
+		echo "Trimming of $s is now complete."
 	fi
-done
-
-SAMPLES2=$(find $trim_dir_out -type f -printf '%f\n')
-
-for s in $SAMPLES2; do
-	samplename="${s%%.*}"
-		if [[ ! -d $trim_final_dir/$samplename.trim.fastq.gz ]]; then
-			echo "Trimming $s beginning..."
-			$CUTADAPT -u 4 -o $trim_final_dir/$samplename.trimm.fastq.gz $trim_dir_out/$s
-			echo "Trimming complete for $s."
-		fi
-
 done
