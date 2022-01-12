8f737bea2008e56b0e2734e0c0300b01244adf95
Destiny Mullens <destiny_mullens@tamu.edu>
2022-01-12 10:29:12 -0600

    Update umi_after_map.sh

--- umi_after_map.sh
+++ umi_after_map.sh
@@ -5,13 +5,14 @@
 # Read config.sh
 . $(dirname $0)/config.sh
 
-mkdir -p $SAVE_LOC/$project_name/deduplicated
-mkdir -p $SAVE_LOC/$project_name/logs/dedplicated 
+mkdir -p $SAVE_LOC/$project_name/$trim_type/deduplicated_files
+mkdir -p $SAVE_LOC/$project_name/$trim_type/indexed_files
+mkdir -p $SAVE_LOC/$project_name/logs/$trim_type/deduplication 
 
 map_dir_in="$SAVE_LOC/$project_name/mapping"
-index_dir_out="$SAVE_LOC/$project_name/deduplicated/indexed_files"
-dedup_dir_out=$SAVE_LOC/$project_name/deduplicated/deduplicated_files
-deduplog="$SAVE_LOC/$project_name/logs/$trim_type"
+index_dir_out="$SAVE_LOC/$project_name/$trim_type/indexed_files"
+dedup_dir_out=$SAVE_LOC/$project_name/$trim_type/deduplicated_files"
+deduplog="$SAVE_LOC/$project_name/logs/$trim_type/deduplication"
 
 SAMPLES=$(find $map_dir_in -type f -printf '%f\n')
 
