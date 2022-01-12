09f2c267c8390731ae35fdeeef2762ca787a25be
Destiny Mullens <destiny_mullens@tamu.edu>
2022-01-12 10:41:11 -0600

    Update qc_second_run.sh

--- secondary_scripts/qc_second_run.sh
+++ secondary_scripts/qc_second_run.sh
@@ -6,8 +6,8 @@
 
 SAMPLES=$(find $mapfiles -type f -printf '%f\n')
 echo "$SAMPLES"
-mkdir -p "$SAVE_LOC/$project_name/qc_reports/reports_after_trim"
-results="$SAVE_LOC/$project_name/qc_reports/reports_after_trim"
+mkdir -p "$SAVE_LOC/$project_name/qc_reports/trimmed"
+results="$SAVE_LOC/$project_name/qc_reports/trimmed"
 for s in $SAMPLES; do
 samplename="${s%%.*}"
 	if [[ ! -d $results/$samplename ]]; then
