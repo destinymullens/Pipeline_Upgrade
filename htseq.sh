027b725eac2013be83d0534c45d979a544dd322e
Destiny Mullens <destiny_mullens@tamu.edu>
2022-01-18 14:53:03 -0600

    Update htseq.sh

--- htseq.sh
+++ htseq.sh
@@ -4,10 +4,12 @@
 . $(dirname $0)/config.sh
 
 mkdir -p "$SAVE_LOC/$project_name/summary/htseq_counts"
+htseq_dir_out="$SAVE_LOC/$project_name/summary/htseq_counts"
 
 if [[ "$strand_num" = "1" ]]; then
 	for i in $htseq_dir_in; do
 		FILE=$(basename $i)
+		mkdir "$htseq_dir_out/$FILE"
 		printf "%s\n" "Counting of $FILE beginning..."
 		$HTSEQ_LOC --stranded=no -f sam -i gene_id --additional-attr=gene_name $i $REF/genes.gtf > $htseq_dir_out/$FILE-htseq.txt
 		printf "%s\n" "Counting of $FILE complete."
@@ -16,7 +18,7 @@ else
 	for i in $htseq_dir_in; do
 		FILE=$(basename $i)
 		printf "%s\n" "Counting of $FILE beginning..."
-		$HTSEQ_LOC --stranded=yes -f sam -i gene_id --additional-attr=gene_name $i $REF/genes.gtf > $htseq_dir_out/$FILE-htseq.txt
+		$HTSEQ_LOC --stranded=yes -f sam -i gene_id --additional-attr=gene_name $i $REF/genes.gtf > $htseq_dir_out/$FILE/$FILE-htseq_output.txt
 		printf "%s\n" "Counting of $FILE complete."
 	done
 fi
