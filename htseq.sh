481f3cb29a0871ce39efaddc4b591b0d33ac8217
Destiny Mullens <destiny_mullens@tamu.edu>
2022-01-24 12:40:22 -0600

    Update htseq.sh

--- htseq.sh
+++ htseq.sh
@@ -12,9 +12,19 @@ for i in $samples; do
 	mkdir "$htseq_dir_out/$FILE"
 	printf "%s\n" "Counting of $FILE beginning..."
 		if [[ "$strand_num" = "1" ]]; then	
-			$HTSEQ_LOC --stranded=no -f sam -i gene_id $i $REF/genes.gtf --additional-attr=gene_name > $htseq_dir_out/$FILE/$FILE-htseq.txt
+			$HTSEQ_LOC --stranded=no \
+			-f sam \	
+			-i gene_id \
+			--additional-attr=gene_name \
+			$i \
+			$REF/genes.gtf  > $htseq_dir_out/$FILE/$FILE-htseq.txt
 			else
-			$HTSEQ_LOC --stranded=yes -f sam -i gene_id $i $REF/genes.gtf --additional-attr=gene_name > $htseq_dir_out/$FILE/$FILE-htseq_output.txt
+			$HTSEQ_LOC --stranded=yes \
+			-f sam \	
+			-i gene_id \
+			--additional-attr=gene_name \
+			$i \
+			$REF/genes.gtf  > $htseq_dir_out/$FILE/$FILE-htseq.txt
 		fi
 	printf "%s\n" "Counting of $FILE complete."
 
@@ -41,11 +51,11 @@ for i in $samples; do
 	## Create list without ERCC genes to get gene counts
 	head -n -$ercc_num $htseq_dir_out/$FILE/$FILE-htseq_gene_counts.log >! $htseq_dir_out/$FILE/$FILE-gene_counts-no_ercc.log
 	## Outputs number of genes with > X number of genes
-	awk '{if ($3>0) print }' $htseq_dir_out/$FILE/htseq.list | wc -l > $htseq_dir_out/$FILE/htseq.0.count
-	awk '{if ($3>1) print }' $htseq_dir_out/$FILE/htseq.list | wc -l > $htseq_dir_out/$FILE/htseq.1count
-	awk '{if ($3>2) print }' $htseq_dir_out/$FILE/htseq.list | wc -l > $htseq_dir_out/$FILE/htseq.2.count
-	awk '{if ($3>3) print }' $htseq_dir_out/$FILE/htseq.list | wc -l > $htseq_dir_out/$FILE/htseq.3.count
-	awk '{if ($3>5) print }' $htseq_dir_out/$FILE/htseq.list | wc -l > $htseq_dir_out/$FILE/htseq.5.count
-	awk '{if ($3>10) print }' $htseq_dir_out/$FILE/htseq.list | wc -l > $htseq_dir_out/$FILE/htseq.10.count
+	awk '{if ($3>0) print }' $htseq_dir_out/$FILE/htseq.list | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.0.count
+	awk '{if ($3>1) print }' $htseq_dir_out/$FILE/htseq.list | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.1count
+	awk '{if ($3>2) print }' $htseq_dir_out/$FILE/htseq.list | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.2.count
+	awk '{if ($3>3) print }' $htseq_dir_out/$FILE/htseq.list | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.3.count
+	awk '{if ($3>5) print }' $htseq_dir_out/$FILE/htseq.list | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.5.count
+	awk '{if ($3>10) print }' $htseq_dir_out/$FILE/htseq.list | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.10.count
 
 done
