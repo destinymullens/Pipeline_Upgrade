91a80a66d8ad0a0448966c9eab4508b41c8c6e23
Destiny Mullens <destiny_mullens@tamu.edu>
2022-03-05 10:55:37 -0600

    Update trim_base.sh

--- trim_base.sh
+++ trim_base.sh
@@ -13,7 +13,9 @@ trim_dir_out="$SAVE_LOC/$project_name/trimmed_files/$trim_type"
 SAMPLES=$(find $trim_dir_in -type f -printf '%f\n')
 
 for s in $SAMPLES; do
+	if [[ ! -f $trim_dir_out/$samplename.trimm.fastq.gz ]]; then
 	samplename="${s%%.*}"
 	$CUTADAPT -u $trim_base_num -j 0 -o $trim_dir_out/$samplename.trimm.fastq.gz $s
 	echo ""
+	fi
 done
