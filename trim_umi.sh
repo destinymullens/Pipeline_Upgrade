bde7a1addc7fce94ec4f253d036998930eed511c
Destiny Mullens <destiny_mullens@tamu.edu>
2022-05-05 13:14:29 -0500

    Update trim_umi.sh

--- trim_umi.sh
+++ trim_umi.sh
@@ -17,14 +17,16 @@ trim_log="$SAVE_LOC/$project_name/logs/$trim_type"
 SAMPLES=$(find $trim_dir_in -type f -printf '%f\n')
 for s in $SAMPLES; do
 	samplename="${s%%.*}"
-	if [[ ! -f $processed_dir_out/$samplename-trimmmed.fastq.gz ]]; then
-		logfile="$samplename.processed.log"
-		stoutfile="$samplename.processed.fastq.gz"
+	logfile="$samplename.processed.log"
+	stoutfile="$samplename.processed.fastq.gz"
+	stoutfile_trimmed="$samplename.trimmed.processed.fastq.gz"
+	if [[ ! -f $processed_dir_out/$stoutfile ]]; then
 		echo "Extracting of UMI's from $s...."
 		$UMI_TOOLS extract --bc-pattern=NNNNNN -I $trim_dir_in/$s --log $trim_log/$logfile -S $processed_dir_out/$stoutfile
 		echo "Extraction of UMI's from $s is now complete."
+		if [[ ! -f $trim_dir_out/$samplename-processed.trim.fastq.gz ]]; then
 		echo "Begining trimming of $s...."
-		cutadapt -u 4 -o $trim_dir_out/$samplename-processed.trim.fastq.gz $processed_dir_out/$stoutfile
-		echo "Trimming of $t is now complete."
+		cutadapt -u 4 -o $trim_dir_out/$stoutfile_trimmed $processed_dir_out/$stoutfile
+		echo "Trimming of $s is now complete."
 		fi
 done
