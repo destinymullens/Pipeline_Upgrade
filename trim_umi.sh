7d8bf4ef99393c1f8d31b92deca5c6c8d1850238
Destiny Mullens <destiny_mullens@tamu.edu>
2022-05-06 08:51:39 -0500

    Update trim_umi.sh

--- trim_umi.sh
+++ trim_umi.sh
@@ -24,10 +24,11 @@ for s in $SAMPLES; do
 		echo "Extracting of UMI's from $s...."
 		$UMI_TOOLS extract --bc-pattern=NNNNNN -I $trim_dir_in/$s --log $trim_log/$logfile -S $processed_dir_out/$stoutfile
 		echo "Extraction of UMI's from $s is now complete."
-		if [[ ! -f $trim_dir_out/$stoutfile_trimmed ]]; then
+	fi
+	if [[ ! -f $trim_dir_out/$stoutfile_trimmed ]]; then
 		echo "Begining trimming of $s...."
 		cutadapt -u 4 -o $trim_dir_out/$stoutfile_trimmed $processed_dir_out/$stoutfile
 		echo "Trimming of $s is now complete."
-		fi
 	fi
+	echo "Extraction of UMI's and trimming complete!"
 done
