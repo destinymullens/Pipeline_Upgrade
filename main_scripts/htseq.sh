5bbed5a2e4afeb41e6882b9fc640c9119c357f5a
Destiny Mullens <destiny_mullens@tamu.edu>
2022-05-09 18:20:14 -0500

    htseq update

--- main_scripts/htseq.sh
+++ main_scripts/htseq.sh
@@ -46,12 +46,12 @@ for i in $samples; do
 	## Create list without ERCC genes to get gene counts
 	head -n -$ercc_num $htseq_dir_out/$FILE/$FILE-htseq_gene_counts.log >! $htseq_dir_out/$FILE/$FILE-gene_counts-no_ercc.log
 	## Outputs number of genes with > X number of genes
-	awk '{if ($3>0) print }' $htseq_dir_out/$FILE/htseq.list | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.0.count
-	awk '{if ($3>1) print }' $htseq_dir_out/$FILE/htseq.list | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.1count
-	awk '{if ($3>2) print }' $htseq_dir_out/$FILE/htseq.list | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.2.count
-	awk '{if ($3>3) print }' $htseq_dir_out/$FILE/htseq.list | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.3.count
-	awk '{if ($3>5) print }' $htseq_dir_out/$FILE/htseq.list | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.5.count
-	awk '{if ($3>10) print }' $htseq_dir_out/$FILE/htseq.list | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.10.count
+	awk '{if ($3>0) print }' $htseq_dir_out/$FILE/$FILE-htseq.txt | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.0.count
+	awk '{if ($3>1) print }' $htseq_dir_out/$FILE/$FILE-htseq.txt | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.1count
+	awk '{if ($3>2) print }' $htseq_dir_out/$FILE/$FILE-htseq.txt | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.2.count
+	awk '{if ($3>3) print }' $htseq_dir_out/$FILE/$FILE-htseq.txt | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.3.count
+	awk '{if ($3>5) print }' $htseq_dir_out/$FILE/$FILE-htseq.txt | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.5.count
+	awk '{if ($3>10) print }' $htseq_dir_out/$FILE/$FILE-htseq.txt | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.10.count
 done
 htseq_version=$($HTSEQ_LOC --version)
 echo "Counting performed using htseq-count version $htseq_version." >> $SAVE_LOC/$project_name/summary/Mapping_Information.txt
