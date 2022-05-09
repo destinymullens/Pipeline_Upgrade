11128a852dc66d584f486fec9bda030f42c0a355
Destiny Mullens <destiny_mullens@tamu.edu>
2022-05-09 09:05:12 -0500

    Update htseq.sh

--- htseq.sh
+++ htseq.sh
@@ -3,8 +3,8 @@
 # Read config.sh
 . $(dirname $0)/config.sh
 
-mkdir -p "$SAVE_LOC/$project_name/summary/htseq_counts"
-htseq_dir_out="$SAVE_LOC/$project_name/summary/htseq_counts"
+mkdir -p "$SAVE_LOC/$project_name/htseq_counts"
+htseq_dir_out="$SAVE_LOC/$project_name/htseq_counts"
 samples=$(ls $htseq_dir_in/*)
 
 for i in $samples; do
@@ -12,9 +12,9 @@ for i in $samples; do
 	mkdir -p "$htseq_dir_out/$FILE"
 	printf "%s\n" "Counting of $FILE beginning..."
 		if [[ "$strand_num" = "1" ]]; then	
-			$HTSEQ_LOC $i $species_location/genes.gtf --stranded=no -f sam -i gene_name --additional-attr=gene_id > $htseq_dir_out/$FILE-htseq.txt
+			$HTSEQ_LOC $i $species_location/genes.gtf --stranded=no -f sam -i gene_name --additional-attr=gene_id > $htseq_dir_out/$FILE/$FILE-htseq.txt
 			else
-			$HTSEQ_LOC --stranded=yes -f sam -i gene_id --additional-attr=gene_name $i $species_location/genes.gtf  > $htseq_dir_out/$FILE-htseq.txt
+			$HTSEQ_LOC --stranded=yes -f sam -i gene_id --additional-attr=gene_name $i $species_location/genes.gtf  > $htseq_dir_out/$FILE/$FILE-htseq.txt
 		fi
 	printf "%s\n" "Counting of $FILE complete."
 
