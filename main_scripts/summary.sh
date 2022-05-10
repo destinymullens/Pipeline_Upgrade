a3ce73a312f73a0341fe175db5d22bdb45dad47c
Destiny Mullens <destiny_mullens@tamu.edu>
2022-05-10 14:05:57 -0500

    Update summary.sh

--- main_scripts/summary.sh
+++ main_scripts/summary.sh
@@ -9,27 +9,35 @@ project_name="Zion_Write_Test"
 htseq_dir_out="$SAVE_LOC/$project_name/htseq_counts"
 samples=$(ls $htseq_dir_out/*/*.txt)
 summary_dir="$SAVE_LOC/$project_name/summary"
-final_counts_file="$project_name-counts.csv"
+counts_file="$project_name-counts.csv"
 mapping_logs="$SAVE_LOC/$project_name/logs/mapping"
-overall_summary_file="$SAVE_LOC/$project_name/summary/$project_name-summary.csv"
+summary_file="$SAVE_LOC/$project_name/summary/$project_name-summary.csv"
+
 
 ### Merge htseq count files into one counts csv file
-n=0
 for i in $samples; do
-	FILE=$(basename $i)
-	echo -e "ID\t $i" > $i-tmp.txt
-	head -n-1 $i | cut -f 1,2 | sort -k1 >> $i-temp.txt
-	((n++))
-	paste $i > $htseq_dir_out/tmpOK
-	rm -f $i-tmp.txt
-	c="-f1"
+	ID=$(echo "$i" | cut -d "-" -f1)
+	if [[ ! -f $counts_file ]]; then
+		printf "%s\t" "Gene Name" > GeneName-tmp.txt
+		awk '{print $1}' $i >> GeneName-tmp.txt
+		printf "%s\t" "Gene ID" > GeneID.txt
+		awk '{print $2}' $i >> GeneID.txt
+		printf "%s\t" "$ID" > $i-tmp.txt
+		awk '{print $3}' $i >> $i-tmp.txt
+		paste GeneID.txt GeneName-tmp.txt $i-tmp >> $counts_file
+	else
+		printf "%s\t" "$ID" > $i-tmp.txt
+		awk '{print $3}' $i >> $i-tmp.txt
+		paste $counts_file $i-tmp >> $counts_file		
+	fi
+rm -f *.tmp.txt
 done
 
-for j in $(seq $n)
- 	do
- 	d=`expr 2 \* $j`
- 	c=$c,$d
-done
+#for j in $(seq $n)
+ #	do
+ #	d=`expr 2 \* $j`
+ #	c=$c,$d
+#done
 
 #echo $c
 cut $c $htseq_dir_out/tmpOK > $SAVE_LOC/$project_name/summary/$final_counts_file
