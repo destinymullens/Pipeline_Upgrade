a07953bfa1edf265d2157e71473195b1e21556e9
Destiny Mullens <destiny_mullens@tamu.edu>
2022-05-11 15:43:02 -0500

    Update Summary Information

--- main_scripts/map_test_exfoliome.sh
+++ main_scripts/map_test_exfoliome.sh
@@ -15,7 +15,7 @@ test_map_logs="$SAVE_LOC/$project_name/logs/test_mapping"
 
 
 test_mappings=$(ls $mapfiles | shuf -n 3)
-SUMMARY="$test_map_logs/Test_mapping_results.txt"
+test_summary="$test_map_logs/Test_mapping_results.txt"
 
 #printf "%s\n" "Test mappings are: $test_mappings"
 
@@ -59,13 +59,13 @@ if [[ ! -f $SAVE_LOC/$project_name/mapping_parameter.txt ]]; then
 				MULTI_MAP_READS=$(cat $test_map_logs/$FILE-$MAPPING-Results.log | head -9 | tail -1 | cut -d " " -f5)
 				ALIGNMENT_RATE=$(cat $test_map_logs/$FILE-$MAPPING-Results.log | head -10 | tail -1 | cut -d " " -f1)
 
-				printf "%s\t" "$FILE" >> $SUMMARY ## Print sample name to summary	
-				printf "%s\t" "$MAPPING" >> $SUMMARY ## Print mapping to summary
-				printf "%s\t" "$TOTAL_READS" >> $SUMMARY ## Print Total reads to summary
-				printf "%s\t" "$SINGLE_MAPPED_READS" >> $SUMMARY ## Print single mapped reads to summary
-				printf "%s\t" "$UNMAPPED_READS" >> $SUMMARY ## Print unmapped reads to summary 
-				printf "%s\t" "$MULTI_MAP_READS" >> $SUMMARY ## Print multimapped reads to summary
-				printf "%s\n" "$ALIGNMENT_RATE" >> $SUMMARY ## Print alignment rate to summary
+				printf "%s\t" "$FILE" >> $test_summary ## Print sample name to summary	
+				printf "%s\t" "$MAPPING" >> $test_summary ## Print mapping to summary
+				printf "%s\t" "$TOTAL_READS" >> $test_summary ## Print Total reads to summary
+				printf "%s\t" "$SINGLE_MAPPED_READS" >> $test_summary ## Print single mapped reads to summary
+				printf "%s\t" "$UNMAPPED_READS" >> $test_summary ## Print unmapped reads to summary 
+				printf "%s\t" "$MULTI_MAP_READS" >> $test_summary ## Print multimapped reads to summary
+				printf "%s\n" "$ALIGNMENT_RATE" >> $test_summary ## Print alignment rate to summary
 
 				B=$((B+1))
 			done
@@ -76,7 +76,7 @@ if [[ ! -f $SAVE_LOC/$project_name/mapping_parameter.txt ]]; then
 	done
 
 
-exfoliome_mapping_parameter=$(awk '$7>max[$1]{max[$1]=$7; row[$1]=$0} END{for (i in row) print row[i]}' $SUMMARY | cut -f 2 | grep -v "^\s*$" | sort | uniq -c | sort -bnr | head -1 | cut -c 9-13)
+exfoliome_mapping_parameter=$(awk '$7>max[$1]{max[$1]=$7; row[$1]=$0} END{for (i in row) print row[i]}' $test_summary | cut -f 2 | grep -v "^\s*$" | sort | uniq -c | sort -bnr | head -1 | cut -c 9-13)
 echo "$exfoliome_mapping_parameter" > $SAVE_LOC/$project_name/mapping_parameter.txt
 echo "Test mapping complete and $exfoliome_mapping_parameter is the best mapping option."
 #echo "Cleaning up files..."
