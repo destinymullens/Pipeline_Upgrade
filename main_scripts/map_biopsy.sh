50667e4fc6b51e8c488996b516157fd604891459
Destiny Mullens <destiny_mullens@tamu.edu>
2022-07-13 10:24:34 -0500

    Update map_biopsy.sh

--- main_scripts/map_biopsy.sh
+++ main_scripts/map_biopsy.sh
@@ -26,41 +26,34 @@ for s in $SAMPLES; do
   mkdir -p $outputdir
   hostrefdir=$species_location
 
-  echo "##################################################################"
-  echo "Processing file $samplename..."
+echo "##################################################################"
+echo "Processing file $samplename..."
 
-
+if [[ ! -d $outputdir/$samplename ]]; then
   if [[ "$strand_num" = "1" ]]; then
-    echo "Mapping single-end biopsy now.."
-    if [[ ! -d $outputdir/$samplename ]]; then
-    	 for i in $(ls $map_dir_in); do
-		    echo -n "Running STAR against host reference ... "
-#        pushd $outputdir/star/$species
-      	$STAR --genomeDir $species_location/STAR --readFilesCommand $read_cmd $map_dir_in/$i --runThreadN $THREADS --sjdbGTFfile $species_location/genes.gtf --outSAMtype BAM Unsorted --genomeLoad NoSharedMemory --outFileNamePrefix $outputdir/$samplename
-      	echo "Done."
-      	echo -n "Filtering STAR host alignments... "
-#        popd
-      	echo "Done."
-	     done
-    fi
-    echo "##################################################################"
+    for i in $(ls $map_dir_in); do
+      echo -n "Running STAR against host reference ... "
+#      pushd $outputdir/star/$species
+      $STAR --genomeDir $species_location/STAR --readFilesCommand $read_cmd $map_dir_in/$i --runThreadN $THREADS --sjdbGTFfile $species_location/genes.gtf --outSAMtype BAM Unsorted --genomeLoad NoSharedMemory --outFileNamePrefix $outputdir/$samplename
+      echo "Done."
+      echo -n "Filtering STAR host alignments... "
+#      popd
+      echo "Done."
     done
   else
-  echo "Mapping paired-end biopsy now.."
-    if [[ ! -d $outputdir/$samplename ]]; then
-      for i in $(ls $map_dir_in/*R1*); do
-        readsearch=$(echo $i | cut -c 1-10)
-        read1=$i
-        read2=$(ls $map_dir_in/$readsearch*R2*)
-        echo -n "Running STAR against host reference ... "
-#       pushd $outputdir/star/$species
-        $STAR --genomeDir $species_location/STAR --readFilesCommand $read_cmd $map_dir_in/$read1 $map_dir_in/$read2  --runThreadN $THREADS --sjdbGTFfile $species_location/genes.gtf --outSAMtype BAM Unsorted --genomeLoad NoSharedMemory --outFileNamePrefix $outputdir/$samplename
-        echo "Done."
-        echo -n "Filtering STAR host alignments... "
+    for i in $(ls $map_dir_in/*R1*); do
+      readsearch=$(echo $i | cut -c 1-10)
+      read1=$i
+      read2=$(ls $map_dir_in/$readsearch*R2*)
+      echo -n "Running STAR against host reference ... "
+#      pushd $outputdir/star/$species
+      $STAR --genomeDir $species_location/STAR --readFilesCommand $read_cmd $map_dir_in/$read1 $map_dir_in/$read2  --runThreadN $THREADS --sjdbGTFfile $species_location/genes.gtf --outSAMtype BAM Unsorted --genomeLoad NoSharedMemory --outFileNamePrefix $outputdir/$samplename
+      echo "Done."
+      echo -n "Filtering STAR host alignments... "
 #      popd
-        echo "Done."
-     done
-    fi
-    echo "##################################################################"
-  done
-done
+      echo "Done."
+    done
+  fi
+  echo "Mapping for $samplename is already complete."
+fi
+echo "##################################################################"
