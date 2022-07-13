8c01eb208496a22ae77eed67acbe52c93d48af20
Destiny Mullens <destiny_mullens@tamu.edu>
2022-07-13 10:48:48 -0500

    Biopsy script updates

--- main_scripts/map_biopsy.sh
+++ main_scripts/map_biopsy.sh
@@ -8,13 +8,11 @@ hostrefdir=$species_location
 if [[ "$trim_type" = "untrimmed" ]]; then
   if [[ "$concat_response" = "1" ]]; then
     map_dir_in="$SAVE_LOC/$project_name/concat"
-    read_cmd="bunzip2 -c"
   else
     map_dir_in="$file_location"
   fi
 else
  	map_dir_in="$SAVE_LOC/$project_name/trimmed_files/$trim_type"
-	read_cmd="gunzip -c"
 fi
 
 map_dir_out="$SAVE_LOC/$project_name/mapping"
@@ -30,29 +28,23 @@ for s in $SAMPLES; do
   if [[ ! -d $outputdir/$samplename ]]; then
     if [[ "$strand_num" = "1" ]]; then
       for i in $(ls $map_dir_in); do
-        echo -n "Running STAR against host reference ... "
 #       pushd $outputdir/star/$species
-        $STAR --genomeDir $species_location/STAR --readFilesCommand $read_cmd $map_dir_in/$i --runThreadN $THREADS --sjdbGTFfile $species_location/genes.gtf --outSAMtype BAM Unsorted --genomeLoad NoSharedMemory --outFileNamePrefix $outputdir/$samplename
-        echo "Done."
-        echo -n "Filtering STAR host alignments... "
+        $STAR --genomeDir $species_location/STAR --readFilesCommand gunzip -c $map_dir_in/$i --runThreadN $THREADS --sjdbGTFfile $species_location/genes.gtf --outSAMtype BAM Unsorted --genomeLoad NoSharedMemory --outFileNamePrefix $outputdir/$samplename
 #       popd
-        echo "Done."
+        echo -n "Mapping for $samplename is complete."
       done
     else
       for i in $(ls $map_dir_in/*R1*); do
         readsearch=$(echo $i | cut -c 1-10)
         read1=$i
         read2=$(ls $map_dir_in/$readsearch*R2*)
-        echo -n "Running STAR against host reference ... "
 #       pushd $outputdir/star/$species
-        $STAR --genomeDir $species_location/STAR --readFilesCommand $read_cmd $map_dir_in/$read1 $map_dir_in/$read2  --runThreadN $THREADS --sjdbGTFfile $species_location/genes.gtf --outSAMtype BAM Unsorted --genomeLoad NoSharedMemory --outFileNamePrefix $outputdir/$samplename
-        echo "Done."
-        echo -n "Filtering STAR host alignments... "
+        $STAR --genomeDir $species_location/STAR --readFilesCommand gunzip -c $map_dir_in/$read1 $map_dir_in/$read2  --runThreadN $THREADS --sjdbGTFfile $species_location/genes.gtf --outSAMtype BAM Unsorted --genomeLoad NoSharedMemory --outFileNamePrefix $outputdir/$samplename
 #       popd
-        echo "Done."
+        echo -n "Mapping for $samplename is complete."
       done
     fi
-    echo "Mapping for $samplename is already complete."
+    echo -n "Mapping for $samplename is already complete."
   fi
   echo "##################################################################"
 done
