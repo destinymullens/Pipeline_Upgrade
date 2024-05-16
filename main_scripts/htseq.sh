a3183320e3c6f7e3a77b7a71f611d1a8eae0f5f6
Destiny Mullens <destiny_mullens@tamu.edu>
2024-05-16 09:27:19 -0500

    Update htseq.sh

--- main_scripts/htseq.sh
+++ main_scripts/htseq.sh
@@ -9,21 +9,27 @@ htseq_dir_out="$SAVE_LOC/$project_name/htseq_counts"
 samples=$(ls $htseq_dir_in/*am)
 
 summary_file="$SAVE_LOC/$project_name/summary/$project_name-htseq-metrics.csv"
-	printf "%s\t" "Sample Name" >> $summary_file ## Print sample name to summary  
-	printf "%s\t" "Mitochondrial Reads" >> $summary_file ## Print sample name to summary	
-	printf "%s\t" "Genes > 0 Reads" >> $summary_file ## Print sample name to summary
-	printf "%s\t" "Genes > 1 Reads" >> $summary_file ## Print sample name to summary  
-	printf "%s\n" "Genes > 2 Reads" >> $summary_file ## Print sample name to summary  
-	
+## Creates headers for summary
+printf "%s\t" "Sample Name" >> $summary_file ## Print sample name to summary  
+printf "%s\t" "Mitochondrial Reads" >> $summary_file ## Print sample name to summary	
+printf "%s\t" "Genes > 0 Reads" >> $summary_file ## Print sample name to summary
+printf "%s\t" "Genes > 1 Reads" >> $summary_file ## Print sample name to summary  
+printf "%s\n" "Genes > 2 Reads" >> $summary_file ## Print sample name to summary  
+
+mkdir -p "$htseq_dir_out/sam_files"
+mkdir -p "$htseq_dir_out/counts"
+mkdir -p "$htseq_dir_out/summary"
+mkdir -p "$htseq_dir_out/temp"
+
 for i in $samples; do
 	FILE=$(basename $i)
-	if [[ ! -d $htseq_dir_out/$FILE ]]; then
-		mkdir -p "$htseq_dir_out/$FILE"
-		printf "%s\n" "Counting of $FILE beginning..."
+	if [[ ! -d ${htseq_dir_out}/${FILE} ]]; then
+		#mkdir -p "$htseq_dir_out/$FILE"
+		printf "%s\n" "Counting of ${FILE} beginning..."
 			if [[ "$strand_num" = "1" ]]; then	
-				$HTSEQ_LOC $i $species_location/genes.gtf --stranded=no -m intersection-strict -f sam -i gene_id --additional-attr=gene_name -o $htseq_dir_out/$FILE/$FILE-htseq.sam > $htseq_dir_out/$FILE/$FILE-htseq.txt
+				$HTSEQ_LOC $i ${species_location}/genes.gtf --stranded=no -m intersection-strict -f sam -i gene_id --additional-attr=gene_name -o ${htseq_dir_out}/sam_files/${FILE}-htseq.sam -c ${htseq_dir_out}/counts/${FILE}-htseq.csv
 			else
-				$HTSEQ_LOC --stranded=yes -m intersection-strict -f sam -i gene_id --additional-attr=gene_name $i $species_location/genes.gtf -o $htseq_dir_out/$FILE/$FILE-htseq.sam > $htseq_dir_out/$FILE/$FILE-htseq.txt
+				$HTSEQ_LOC --stranded=yes -m intersection-strict -f sam -i gene_id --additional-attr=gene_name $i ${species_location}/genes.gtf -o ${htseq_dir_out}/sam_files/${FILE}-htseq.sam -c ${htseq_dir_out}/counts/${FILE}-htseq.csv
 			fi
 		printf "%s\n" "Counting of $FILE complete."
 	else
@@ -33,35 +39,24 @@ for i in $samples; do
 #### This section is saving the htseq counts file & individual metrics for overall summary output
 
 	## Htseq count overall metrics
-	tail -5 $htseq_dir_out/$FILE/$FILE-htseq.txt > $htseq_dir_out/$FILE/$FILE-htseq_summary.log
-
-	## Outputs only the gene counts without overall metrics at end of file (for easier merging with other gene counts later)
-	head -n -5 $htseq_dir_out/$FILE/$FILE-htseq.txt > $htseq_dir_out/$FILE/$FILE-gene_counts_all.txt
-
-#	## Creates list of only ERCC genes
-#	grep "^ERCC-" $htseq_dir_out/$FILE/$FILE-htseq.txt > $htseq_dir_out/$FILE/$FILE-ERCC.list
-	
-#	## Gets count of ERCC reads
-#	awk '{ sum+=$3 } END { print sum }' $htseq_dir_out/$FILE/$FILE-ERCC.list > $htseq_dir_out/$FILE/$FILE-ERCC.count
+	tail -5 ${htseq_dir_out}/counts/${FILE}-htseq.csv > ${htseq_dir_out}/summary/${FILE}-htseq_summary.txt
 
 	## Creates list of only mitochondrial genes
-	grep -i "MT-" $htseq_dir_out/$FILE/$FILE-htseq.txt > $htseq_dir_out/$FILE/$FILE-MITO.list
+	grep -i "MT-" ${htseq_dir_out}/counts/${FILE}-htseq.csv > ${htseq_dir_out}/${FILE}-MTreads.csv
+	
 	## Gets count of mitocondrial reads
-	awk '{ sum+=$3 } END { print sum }' $htseq_dir_out/$FILE/$FILE-MITO.list > $htseq_dir_out/$FILE/$FILE-MITO.count
-
-
+	awk '{ sum+=$3 } END { print sum }' ${htseq_dir_out}/counts/${FILE}-MTreads.csv > ${htseq_dir_out}/${FILE}-htseq.MT.count
 	## Outputs number of genes with > X number of genes
-	awk '{if ($3>0) print }' $htseq_dir_out/$FILE/$FILE-gene_counts_all.txt | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.0.count
-	awk '{if ($3>1) print }' $htseq_dir_out/$FILE/$FILE-gene_counts_all.txt | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.1.count
-	awk '{if ($3>2) print }' $htseq_dir_out/$FILE/$FILE-gene_counts_all.txt | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.2.count
-#	awk '{if ($3>5) print }' $htseq_dir_out/$FILE/$FILE-gene_counts-no_ercc.txt | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.5.count
-#	awk '{if ($3>10) print }' $htseq_dir_out/$FILE/$FILE-gene_counts-no_ercc.txt | wc -l > $htseq_dir_out/$FILE/$FILE-htseq.10.count
+	awk '{if ($3>0) print }' ${htseq_dir_out}/counts/${FILE}-htseq.csv | wc -l > ${htseq_dir_out}/${FILE}-htseq.0.count
+	awk '{if ($3>1) print }' ${htseq_dir_out}/counts/${FILE}-htseq.csv | wc -l > ${htseq_dir_out}/${FILE}-htseq.1.count
+	awk '{if ($3>2) print }' ${htseq_dir_out}/counts/${FILE}-htseq.csv | wc -l > ${htseq_dir_out}/${FILE}-htseq.2.count
 
-	printf "%s\t" "$FILE" >> $summary_file ## Print sample name to summary   
-    printf "%s\t" $(cat $htseq_dir_out/$FILE/$FILE-MITO.count) >> $summary_file ## Print Total mito reads to summary
-    printf "%s\t" $(cat $htseq_dir_out/$FILE/$FILE-htseq.0.count) >> $summary_file ## Print Total > 0 reads to summary
-    printf "%s\t" $(cat $htseq_dir_out/$FILE/$FILE-htseq.1.count) >> $summary_file ## Print Total > 1 reads to summary
-    printf "%s\n" $(cat $htseq_dir_out/$FILE/$FILE-htseq.2.count) >> $summary_file ## Print Total > 2 reads to summary
+	printf "%s\t" "${FILE}" >> $summary_file ## Print sample name to summary   
+    printf "%s\t" $(cat ${htseq_dir_out}/${FILE}-htseq.MT.count) >> $summary_file ## Print Total mito reads to summary
+    printf "%s\t" $(cat ${htseq_dir_out}/${FILE}-htseq.0.count) >> $summary_file ## Print Total > 0 reads to summary
+    printf "%s\t" $(cat ${htseq_dir_out}/${FILE}-htseq.1.count) >> $summary_file ## Print Total > 1 reads to summary
+    printf "%s\n" $(cat ${htseq_dir_out}/${FILE}-htseq.2.count) >> $summary_file ## Print Total > 2 reads to summary
+    rm ${htseq_dir_out}/${FILE}-*.count
 done
 
 htseq_version=$($HTSEQ_LOC --version)
