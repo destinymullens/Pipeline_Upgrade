141aef0d962881262579e37fb12b66671b1f50e7
Destiny Mullens <destiny_mullens@tamu.edu>
2024-06-05 16:25:19 -0500

    Changes to be committed: 	modified:   Run_Chapkin_Lab_Pipeline.sh 	modified:   config-trinity.sh 	modified:   config.sh 	modified:   main_scripts/Pipeline_Execute.sh 	modified:   main_scripts/concat_run.sh 	modified:   main_scripts/htseq.sh 	modified:   main_scripts/map_biopsy.sh 	modified:   main_scripts/map_exfoliome_default.sh 	modified:   main_scripts/map_exfoliome_with_parameters.sh 	modified:   main_scripts/map_test_exfoliome.sh 	modified:   main_scripts/qc_run.sh 	modified:   main_scripts/summary.sh 	modified:   main_scripts/trim_base.sh 	modified:   main_scripts/trim_quality.sh 	modified:   main_scripts/trim_umi.sh 	modified:   main_scripts/umi_after_map.sh 	modified:   misc_scripts/concat_preview.sh

--- config.sh
+++ config.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-## Git Test
+## Trinity config
 
 ## These are the variables that are the same across all of the various scripts in the pipeline. 
-## Change necessary locations for program installtions, reference locations & threads available
+## Change necessary locations for program installations, reference locations & threads available
 
 
 #The following variables are for the program file paths.
 #If the location is ~/.local/ it needs to be written as $HOME/.local/
-FASTQC="/opt/programs/bin/FastQC/fastqc"
-STAR="/opt/programs/bin/STAR-2.6.0a/bin/Linux_x86_64/STAR"
-BOWTIE="/usr/bin/bowtie2" #When installed using apt-get install it is located here.
-HTSEQ_LOC="/usr/local/bin/htseq-count"
-UMI_TOOLS="/usr/local/bin/umi_tools"
-CUTADAPT="/usr/local/bin/cutadapt"
-SAMTOOLS="/usr/bin/samtools"
+FASTQC="/bin/fastqc"
+STAR="/bin/STAR"
+BOWTIE="/bin/bowtie2" #When installed using apt-get install it is located here.
+
+HTSEQ_LOC="htseq-count"
+UMI_TOOLS="umi_tools"
+CUTADAPT="/bin/cutadapt"
+SAMTOOLS="/bin/samtools"
 
 #The following variable is for the parent folder that the reference genomes are saved in.
 REF_LOC="/mnt/matrix/roo/refs"
-#The following variable is for the parent folder that projects are saved in.
-#SAVE_LOC="/mnt/zion/Destiny_Pipeline_Test"
+
 #The following variable is for the number of threads available on the computer you are using.
-THREADS=20
+THREADS=30
