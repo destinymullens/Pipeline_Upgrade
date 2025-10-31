4ccfc0cbe9e7086ff7f7ee0bfe18c8059af80015
Destiny Mullens <destiny_mullens@tamu.edu>
2025-10-31 13:21:41 -0500

    Update concat_run.sh

--- main_scripts/concat_run.sh
+++ main_scripts/concat_run.sh
@@ -10,16 +10,15 @@ SAMPLES=$(find ${file_location} -type f -printf '%f\n' | cut -c 1-${concat_lengt
 for s in ${SAMPLES}; do
 	NEW=$s.fastq.gz
     if [[ ! -s ${SAVE_LOC}/concat/$NEW ]]; then
-        touch ${SAVE_LOC}/concat/$NEW
         FILES=$(find ${file_location} -iname "${s}"*.gz)
-        
+        touch ${SAVE_LOC}/concat/$NEW
         echo "For sample $s, concatenating input files:"
         for f in ${FILES}; do
             echo ${f}
-        done
+        
         echo "into new file: $SAVE_LOC/concat/$NEW"; echo ""
-    	zcat ${FILES} >> ${SAVE_LOC}/concat/${NEW}
+    	zcat ${f} >> ${SAVE_LOC}/concat/${NEW}
         echo "... Done."; echo ""; echo ""
-    
+        done
     fi
 done
