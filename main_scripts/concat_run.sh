2d985f747ee8dd40c4a84cfdf0bd40d22ca6e26a
Destiny Mullens <destiny_mullens@tamu.edu>
2025-11-04 13:15:45 -0600

    Update concat_run.sh

--- main_scripts/concat_run.sh
+++ main_scripts/concat_run.sh
@@ -4,27 +4,6 @@
 #. $(dirname $0)/../config.sh
 source ${SAVE_LOC}/${project_name}/config.sh
 
-##Import sample list from file location
-#SAMPLES=$(find ${file_location} -type f -printf '%f\n' | cut -c 1-${concat_length} | sort | uniq)
-#mkdir 
-#for s in ${SAMPLES}; do
-#	NEW=$s.fastq.gz
-#   if [[ ! -s ${SAVE_LOC}/concat/$NEW ]]; then
-#        FILES=$(find ${file_location} -iname "${s}"*.gz)
-#        touch ${SAVE_LOC}/concat/$NEW
-#        echo "For sample $s, concatenating input files:"
-#        for f in ${FILES}; do
-#            echo ${f}        
-#        echo "into new file: $SAVE_LOC/concat/$NEW"; echo ""
-#    	zcat ${FILES} > ${SAVE_LOC}/concat/${NEW}
-#        echo "... Done."; echo ""; echo ""
-#        done
-#    fi
-#done
-
-
-#!/bin/bash
-# Concatenate files in a directory based on shared filename prefix
 
 # Exit on error
 set -e
@@ -33,11 +12,6 @@ input_dir="$file_location"
 prefix_length="$concat_length"
 output_dir="${SAVE_LOC}/${project_name}/concat"
 
-# Make sure directories exist
-if [ ! -d "$file_location" ]; then
-    echo "Error: Input directory '$file_location' not found."
-    exit 1
-fi
 
 mkdir -p "$output_dir"
 
