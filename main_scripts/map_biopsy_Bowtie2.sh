82b2879a2c3adc6cc14b21ef490939150abaf168
Destiny Mullens <destiny_mullens@tamu.edu>
2025-11-07 08:00:27 -0600

    Update map_biopsy_Bowtie2.sh

--- main_scripts/map_biopsy_Bowtie2.sh
+++ main_scripts/map_biopsy_Bowtie2.sh
@@ -6,29 +6,34 @@ source ${SAVE_LOC}/${project_name}/config.sh
 # Exit on error
 set -e
 
-MAP_FILES=$(ls ${map_dir_in}/*R1*)
+
 SUMMARY="${SAVE_LOC}/${project_name}/summary/$project_name-Mapping_summary.csv"
 
 ##### RUN BOWTIE2 #########
-for m in $MAP_FILES; do
-	FILE=$(basename $m)
-	if [[ ! -f ${mapping_dir_out}/${FILE}.sam ]]; then
-		echo "Mapping of ${FILE} is already complete!"
-	else
-		printf "%s\n" "Mapping of ${FILE} beginning..."
 
-		if [[ "${strand_num}" = "1" ]]; then
+if [[ "${strand_num}" = "1" ]]; then
+
+	for i in $(ls ${map_dir_in}); do
+
+		FILE=$(basename $i)
+		if [[ ! -f ${mapping_dir_out}/${FILE}.sam ]]; then
+			echo "Mapping of ${FILE} is already complete!"
+		else
+			printf "%s\n" "Mapping of ${FILE} beginning..."
 			${BOWTIE} -x ${species_location}/bowtie2/${species} --threads ${THREADS} -U ${mapfiles}/${m} --time -S ${mapping_dir_out}/${FILE}.sam 2> ${mapping_logs}/${FILE}-Results.log
 			printf "%s\n" "✅ Mapping of ${FILE} complete."
-		else
-			m2="${m/_R1/_R2}"
-			${BOWTIE} -x ${species_location}/bowtie2/${species} --threads ${THREADS} -1 ${mapfiles}/${m} -2 ${mapfiles}/${m2} --time -S ${mapping_dir_out}/${FILE}.sam 2> ${mapping_logs}/${FILE}-Results.log
-			printf "%s\n" "✅ Mapping of ${FILE} complete."
-
 		fi
+	done
 
-	fi
-done
+else
+	for i in $(ls ${map_dir_in}/*R1*); do
+        read1=${i}
+        readsearch=$(echo ${i} | cut -d_ -f1)
+        read2=$(ls ${map_dir_in}/${readsearch}*R2*)
+		${BOWTIE} -x ${species_location}/bowtie2/${species} --threads ${THREADS} -1 ${mapfiles}/${read1} -2 ${mapfiles}/${read2} --time -S ${mapping_dir_out}/${FILE}.sam 2> ${mapping_logs}/${FILE}-Results.log
+			printf "%s\n" "✅ Mapping of ${FILE} complete."
+	done
+fi
  
 bowtie_version=$(${BOWTIE} --version | cut -d " " -f3)
 echo "Mapping performed using Bowtie2 version ${bowtie_version} with default settings." >> ${mapping_information}
