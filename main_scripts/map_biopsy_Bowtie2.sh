aad17fe835b7809b38014b48e72654812073a125
Destiny Mullens <destiny_mullens@tamu.edu>
2025-11-21 15:03:17 -0600

    Update map_biopsy_Bowtie2.sh

--- main_scripts/map_biopsy_Bowtie2.sh
+++ main_scripts/map_biopsy_Bowtie2.sh
@@ -6,34 +6,51 @@ source ${project_dir}/config.sh
 # Exit on error
 set -e
 
+## Create directories to save various files in
 
-SUMMARY="${project_dir}/summary/$project_name-Mapping_summary.csv"
+map_log_dir="${project_dir}/logs/mapping"
+mkdir -p ${map_log_dir}
 
-##### RUN BOWTIE2 #########
+## Create list for files that need to be mapped
+SampleList=$(ls ${map_dir_in})
 
 if [[ "${strand_num}" = "1" ]]; then
 
-	for i in $(ls ${mapfiles}); do
+	for Sample in ${SampleList}; do
+		SampleName="${Sample%%.*}"
+		map_file_out="${map_dir_out}/${SampleName}.sam"
+		map_log_file="${map_log_dir}/${SampleName}.results.log"
 
-		FILE=$(basename $i)
-		if [[ ! -f ${mapping_dir_out}/${FILE}.sam ]]; then
-			echo "Mapping of ${FILE} is already complete!"
+		if [[ ! -f ${map_file_out} ]]; then
+			echo "Alignment of ${SampleName} is already complete!"
 		else
-			printf "%s\n" "Mapping of ${FILE} beginning..."
-			${BOWTIE} -x ${species_location}/bowtie2/${species} --threads ${THREADS} -U ${mapfiles}/${m} --time -S ${mapping_dir_out}/${FILE}.sam 2> ${mapping_logs}/${FILE}-Results.log
-			printf "%s\n" "Mapping of ${FILE} complete."
+			echo "Beginning alignment of $SampleName..."
+			${BOWTIE} -x ${Bowtie2_ref} --threads ${THREADS} -U ${map_dir_in}/${Sample} --time -S ${map_file_out} 2> ${map_log_file}
+			echo "Optimized alignment of $SampleName complete."
 		fi
 	done
-
 else
-	for i in $(ls ${mapfiles}/*R1*); do
-        read1=${i}
-        readsearch=$(echo ${i} | cut -d_ -f1)
+	for Sample in $(ls ${map_dir_in}/*R1*); do
+        SampleName="${Sample%%.*}"
+        map_file_out="${map_dir_out}/${SampleName}.sam"
+		map_log_file="${map_log_dir}/${SampleName}.results.log"
+        read1=${Sample}
+        readsearch=$(echo ${Sample} | cut -d_ -f1)
         read2=$(ls ${mapfiles}/${readsearch}*R2*)
-		${BOWTIE} -x ${species_location}/bowtie2/${species} --threads ${THREADS} -1 ${mapfiles}/${read1} -2 ${mapfiles}/${read2} --time -S ${mapping_dir_out}/${FILE}.sam 2> ${mapping_logs}/${FILE}-Results.log
+        map_file_out="${map_dir_out}/${SampleName}.sam"
+		map_log_file="${map_log_dir}/${SampleName}.results.log"
+
+		${BOWTIE} -x ${species_location}/bowtie2/${species} --threads ${THREADS} -1 ${map_dir_in}/${read1} -2 ${map_dir_in}/${read2} --time -S ${map_file_out} 2> ${map_log_file}
 			printf "%s\n" "Mapping of ${FILE} complete."
 	done
 fi
-printf "%s\n" "✅ Mapping of all samples complete."
-bowtie_version=$(${BOWTIE} --version | cut -d " " -f3)
-echo "Mapping performed using Bowtie2 version ${bowtie_version} with default settings." >> ${mapping_information}
+
+echo "✅ Alignment of all samples is complete!!!"
+
+bowtie_version=$(${BOWTIE} --version | cut -d " " -f3 | head -1)
+
+## Add Reference information to Mapping Info!!!!
+cat >> "${mapping_information}" <<EOF
+Alignment performed with Bowtie2 ${bowtie_version} with default parameters.
+The reference ${species_ref} was used for sample alignment.
+EOF
